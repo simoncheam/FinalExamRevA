b6e7cfef5c0d34c7b7eacf5ad4e9cbb2f6ba281f
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-01-19 15:11:21 -0500

    commit

--- src/client/views/BookDetail.tsx
+++ src/client/views/BookDetail.tsx
@@ -1,49 +1,83 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Books } from '../client_types';
 import { APIService } from '../services/APIService';
 
 const BookDetail = () => {
 
     let params = useParams();
     const book_id = params.id
+    const nav = useNavigate();
+    const [isLoaded, setIsLoaded] = useState<boolean>(false)
 
-    const [books, setBooks] = useState<Books[]>([])
+
+
+    const [book, setBook] = useState<Books>()
 
     const [bookTitle, setBookTitle] = useState('')
-    const [bookAuthor, setBookAuthor] = useState('')
-    const [bookPrice, setBookPrice] = useState<number>(null)
+    // const [bookAuthor, setBookAuthor] = useState('')
+    // const [bookPrice, setBookPrice] = useState<number>(null)
 
     const [selectedCategoryId, setSelectedCategoryId] = useState<number>()
-
-
+    const [categoryName, setCategoryName] = useState<string>('')
 
 
     useEffect(() => {
 
         //API 1 - set book
         //API stuff
-        APIService(`/api/books `)
+        APIService(`/api/books/${book_id} `)
             .then(data => {
-                data = data[0];
                 // state stuff
-                setBooks(data)
-
+                setBook(data)
+                setSelectedCategoryId(data.categoryid)
                 console.log(data);
+                setIsLoaded(true)
 
                 //API 2 - categories
+                APIService(`/api/categories/${book.categoryid} `)
+                    .then(data => {
+                        setCategoryName(data.name)
+
+
+                    })
+                    .catch(e => {
+                        console.log(e)
+                    })
 
 
             })
             .catch(e => {
                 console.log(e)
             })
-    }, [])
+    }, [isLoaded])
+
+    if (!book || !categoryName) { return <> Loading...</> }
 
 
 
     return <div>
 
         <h1 className="display-3 m-3 text-center"> Book Detail </h1>
+        <div className="row justify-content-center m-2">
+            <div className="col-md-6">
+                <div className="card shadow m-3 p-3">
+                    <h1>{book.title}</h1>
+                    <div className="card-body">
+                        <h5 className="card-title"> 🏷{categoryName} </h5>
+                        <h6 className="card-subtitle mb-2 text-muted">Price: {book.price.toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                        })}</h6>
+                        <p className="card-text">By: {book.author}</p>
+                        <button onClick={() => nav(-1)} className="row btn btn-primary m-2">Go Back </button>
+                        <Link to={`/books/${book.id}/update`} className="row btn btn-warning m-2">Edit </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
 
     </div>;
 };
--- src/client/views/UpdateBook.tsx
+++ src/client/views/UpdateBook.tsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Categories } from '../client_types';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Books, Categories } from '../client_types';
+import { APIService } from '../services/APIService';
 
 const UpdateBook = () => {
+
+    const nav = useNavigate();
+
+    let params = useParams();
+    const book_id = params.id
+
+    const [book, setBook] = useState<Books>()
 
     const [bookTitle, setBookTitle] = useState('')
     const [bookAuthor, setBookAuthor] = useState('')
@@ -14,10 +23,96 @@ const UpdateBook = () => {
 
     useEffect(() => {
 
-        //API 1 - set book
-        //API 2 - categories
+
+        //API 1 - set book - fetch book details
+        APIService(`/api/books/${book_id} `)
+            .then((data: Books) => {
+                // state stuff
+                setBook(data)
+
+                //set auto fill TAPC
+                setBookTitle(data.title)
+                setBookAuthor(data.author)
+                setBookPrice(data.price)
+                setSelectedCategoryId(data.categoryid)
+
+                console.log(data);
+                setIsLoaded(true)
+
+                //API 2 - categories
+                APIService(`/api/categories `)
+                    .then(data => {
+                        setCategories(data)
+
+                    })
+                    .catch(e => {
+                        console.log(e)
+                    })
+
+
+            })
+            .catch(e => {
+                console.log(e)
+            })
+
+
+
 
     }, [])
+
+    if (!book || !categories) { return <> Loading...</> }
+
+
+    const handleDeleteButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+        e.preventDefault();
+
+        if (confirm('are you sure?')) {
+
+            APIService(`/api/books/${book_id}`, 'DELETE')
+                .then(data => {
+
+                    alert('Deleted book!')
+
+                    nav(`/books`)
+
+                })
+                .catch(e => {
+                    console.log(e)
+                })
+        }
+
+
+    }
+
+    const handleSubmitButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+        APIService(`/api/books/${book_id}`, 'PUT', {
+
+            title: bookTitle,
+            author: bookAuthor,
+            price: bookPrice,
+            categoryid: selectedCategoryId
+        })
+            .then(data => {
+                alert('Thanks for the update')
+                nav(`/books`)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    }
+
+
+
+
+    const handleCategoryIdSelectUpdate = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        e.preventDefault();
+        setSelectedCategoryId(Number(e.target.value))
+    }
+
+
 
 
 
@@ -26,6 +121,57 @@ const UpdateBook = () => {
 
     return <div>
         <h1 className="display-3 m-3 text-center"> Update Book </h1>
+
+
+        <main className="container my-5">
+            <section className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card shadow">
+                        <div className="card-header"> Got changes to make?:</div>
+
+                        <div className="card-body">
+                            <h1>Complete updates below: </h1>
+                            <form className="form-group my-2">
+                                <label>Title :</label>
+                                <input className="form-control m-2"
+                                    value={bookTitle}
+                                    placeholder='username'
+                                    type='text'
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserName(e.target.value)}
+                                />
+
+                                <label>email:</label>
+
+                                <input className="form-control m-2"
+                                    value={email}
+                                    placeholder='your email'
+                                    type='text'
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                                />
+
+                                <label>password:</label>
+
+                                <input className="form-control m-2"
+                                    value={password}
+                                    placeholder='password'
+                                    type='password'
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+
+
+                                />
+
+                                <button onClick={handleSubmitButton} className='btn btn-success mt-2'> Register Now</button>
+
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+
+
 
     </div>;
 };
